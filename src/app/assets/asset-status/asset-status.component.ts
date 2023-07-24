9dbbd4abdbb9a334a67fa6989c472a2ac90a48ba
Adelino Ngomacha <adelino.ngomacha@vm.co.mz>
2023-07-24 17:51:29 +0200

    feat: add list technicians page

--- src/app/assets/asset-status/asset-status.component.ts
+++ src/app/assets/asset-status/asset-status.component.ts
@@ -7,9 +7,9 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
       <span class="flex items-center justify-center w-full rounded-full border-2 px-4 py-2" [ngClass]="{
-            'bg-green-200 border-green-500': status === 'in_storage',
-            'bg-yellow-100 border-yellow-500': status === 'in_use',
-            'bg-red-200 border-red-500': status === 'in_maintenance',
+            'bg-green-100 border-green-500': status === 'success',
+            'bg-yellow-100 border-yellow-500': status === 'warning',
+            'bg-red-200 border-red-500': status === 'danger',
         }">
       <ng-content/>
     </span>
