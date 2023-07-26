b1fbbcf016d01e4358cba76b84b573f456786199
Adelino Ngomacha <adelino.ngomacha@vm.co.mz>
2023-07-26 19:46:01 +0200

    feat: add activities page

--- src/app/assets-details/asset-activities/asset-activities.component.ts
+++ src/app/assets-details/asset-activities/asset-activities.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TableComponent} from "../../shared/table/table.component";
+import {AssetStatusComponent} from "../../assets/asset-status/asset-status.component";
 
 @Component({
   selector: 'app-asset-activities',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableComponent, AssetStatusComponent],
   templateUrl: './asset-activities.component.html',
   styles: [
   ]
