5c247f03a8e94519db6c20e98bf558e5792568d4
Adelino Ngomacha <adelino.ngomacha@vm.co.mz>
2023-07-26 19:26:45 +0200

    feat: add Asset Maintenance page

--- src/app/assets-details/asset-maintenance/asset-maintenance.component.ts
+++ src/app/assets-details/asset-maintenance/asset-maintenance.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TableComponent} from "../../shared/table/table.component";
+import {AssetStatusComponent} from "../../assets/asset-status/asset-status.component";
 
 @Component({
   selector: 'app-asset-maintenance',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableComponent, AssetStatusComponent],
   templateUrl: './asset-maintenance.component.html',
   styles: [
   ]
