6a43b4c196936b0d9c4b801532c0f0872052d3d0
Adelino Ngomacha <adelino.ngomacha@vm.co.mz>
2023-07-26 19:20:13 +0200

    feat: add Machine Meters page

--- src/app/assets-details/asset-meters/asset-meters.component.ts
+++ src/app/assets-details/asset-meters/asset-meters.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {TableComponent} from "../../shared/table/table.component";
 
 @Component({
   selector: 'app-asset-meters',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, TableComponent],
   templateUrl: './asset-meters.component.html',
   styles: [
   ]
